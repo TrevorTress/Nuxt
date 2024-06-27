e25b732362022c39467b9838d8cb1f2799444011
Trevor Tress <trevor.tress@outlook.com>
2024-06-27 15:34:22 -0400

    more stuff

--- layouts/default.vue
+++ layouts/default.vue
@@ -6,3 +6,14 @@
 	</Container>
 	<Footer />
 </template>
+
+<style>
+	:root {
+		--safety: #dcc032;
+		--equipment: #dc8923;
+		--maintenance: #0f4ead;
+		--troubleshooting: #920e0e;
+		--drawings: purple;
+		--appendix: grey;
+	}
+</style>
